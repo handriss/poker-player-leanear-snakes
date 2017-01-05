b039b7525276c53a46090412482b28e24cedbf92
handriss <andrashinkel@gmail.com>
2017-01-05 11:12:06 +0100

    refactor cicuska2

--- player.py
+++ player.py
@@ -102,21 +102,21 @@ class Player:
     def get_own_cards(self, game_state):
         for player in game_state['players']:
             try:
-                self.own_cards = player['hole_cards']
+                return player['hole_cards']
             except KeyError:
                 pass
 
 
     def get_community_cards(self, game_state):
         try:
-            self.community_cards = game_state['community_cards']
+            return game_state['community_cards']
         except KeyError:
             return None
 
 
     def betRequest(self, game_state):
-        self.get_own_cards(game_state)
-        self.get_community_cards(game_state)
+        self.own_cards = self.get_own_cards(game_state)
+        self.community_cards = self.get_community_cards(game_state)
 
         if self.community_cards is None:
             if self.check_preflop():
@@ -142,8 +142,8 @@ class Player:
 
 
     def check_high_card(self):
+        pass
         # get highest cards from hand
-
 
         # get highest card from community cards
 
